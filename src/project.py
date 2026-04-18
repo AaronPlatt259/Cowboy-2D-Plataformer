d703da21ff0f9997409f46d432fe990a99316372
Aaron Platt <aaronplatth@gmail.com>
2026-04-18 17:33:06 -0500

    made the Player class

--- src/project.py
+++ src/project.py
@@ -1,3 +1,22 @@
+import pygame
+import random
+
+
+class Player:
+    def __init__(self, pos=(0,0),x_axis=120, y_axis = 100):
+        self.x_axis = x_axis
+        self.y_axis = y_axis
+        self.pos = pos
+        self.img = self.image()
+       
+    def image(self):
+        img = pygame.image.load('cowboy_placeholder.png').convert_alpha()
+        #img = pygame.transform.scale(img, (self.x_axis*2, self.y_axis*2))
+        return img
+
+    def draw(self, surface):
+        surface.blit(self.img, self.pos)
+
 def main():
     pygame.init()
 
