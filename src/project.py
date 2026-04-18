fd92d5339294af17e4e1ce2e7db84cadea2b4c9f
Aaron Platt <aaronplatth@gmail.com>
2026-04-18 18:21:22 -0500

    added Background class so it is easier to bring it to the screen

--- src/project.py
+++ src/project.py
@@ -17,17 +17,31 @@ class Player:
     def draw(self, surface):
         surface.blit(self.img, self.pos)
 
+class Background:
+        def __init__(self, pos=(0,0),x_axis=120, y_axis = 100):
+            self.x_axis = x_axis
+            self.y_axis = y_axis
+            self.pos = pos
+            self.ground = self.image()
+       
+        def image(self):
+            img = pygame.image.load('bg_placeholder.jpg')
+            #img = pygame.transform.scale(img, (self.x_axis*100, self.y_axis*100))
+            return img
+
+        def draw(self, surface):
+            surface.blit(self.ground, self.pos)
 def main():
     pygame.init()
     pygame.display.set_caption("Cowboy Game")
     clock = pygame.time.Clock()
     dt = 0
     resolution = (800, 600)
-    bg = pygame.image.load('bg_placeholder.jpg')
     screen = pygame.display.set_mode(resolution)
 
     #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-    player = Player()
+    player = Player(pos=(0,0))
+    bg = Background(pos=(0,0))
 
     #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     running = True
@@ -36,7 +50,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
     
-    screen.fill(bg)
+    screen.fill('Green')
+    bg.draw(screen)
     player.draw(screen)
     pygame.display.flip()
 
